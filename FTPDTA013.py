90015b2faee233dd3905026c5c75b6de0bb951b1
Po Chien Yang <ypochien@gmail.com>
2016-09-19 15:13:22 +0800

    fixed #1 . 013csv

---
+++ FTPDTA013.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import csv
+
+import HTML
+
+HTML_HEADER = '''
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <style>
+        table {
+            border-collapse: collapse;
+            align: center;
+        }
+        table, td, th {
+            border: 1px solid black;
+        }
+    </style>\r'''
+
+with open('data/FTPDTA013.csv' , mode='r' , encoding='cp950') as csv_file:
+    reader = csv.reader(csv_file)
+    data013 = list(reader)
+
+t = HTML.Table(data013[1:])
+
+with open('out/FTPDTA013.html' , mode='w' , encoding='utf-8') as html_file:
+    html_file.write(HTML_HEADER)
+    html_file.write(data013[0][0])
+    html_file.write(str(t))
